5a03be1d691355c81c6ee69d8b61294ec68180a3
WillY-1 <101967637+WillY-1@users.noreply.github.com>
2022-04-03 12:49:54 +0800

    Update a24.c

--- a24.c
+++ a24.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
-    float c,f;
-    printf("input F:");
-    scanf("%f",&f);
-    c=(f-32)/1.8;
-    printf("C:%2.7f\n",c);
+    float c,f;                        //定义浮点数c,f
+    printf("input F:");               //
+    scanf("%f",&f);                   //输入要转换的华氏度
+    c=(f-32)/1.8;                     //转换公式
+    printf("C:%.2f\n",c);            //输出求得的摄氏度，保留两位数字
 }
