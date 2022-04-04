72c31a1eb7268b2d5bbbe975e4132f82fde4113a
WillY-1 <101967637+WillY-1@users.noreply.github.com>
2022-04-05 01:15:57 +0800

    Update a32.c

--- a32.c
+++ a32.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-main(){
+int main(){
     char a[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     char ch;
     int i;
