e76b21885278bcd2ec0c3e9cf7ae039f2acf7a41
WillY-1 <101967637+WillY-1@users.noreply.github.com>
2022-04-05 00:42:13 +0800

    Add files via upload

---
+++ a33.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+    int a,b,c;
+    printf("input a b c:");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a<b&&b<c) printf("max=%d\nmin=%d",c,a);
+    else if(c<=b&&b<=a) printf("max=%d\nmin=%d",a,c);
+    else if(b<=a&&a<=c) printf("max=%d\nmin=%d",c,b);
+    else if(c<=a&&a<=b) printf("max=%d\nmin=%d",b,c);
+    else if(a<=c&&c<=b) printf("max=%d\nmin=%d",b,a);
+    else if(b<=c&&c<=a) printf("max=%d\nmin=%d",a,b);
+}
