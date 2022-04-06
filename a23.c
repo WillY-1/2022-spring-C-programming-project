7f4f7af91d20183beb79fc910a1154066c08f3f5
WillY-1 <101967637+WillY-1@users.noreply.github.com>
2022-04-06 20:55:51 +0800

    Update a23.c

--- a23.c
+++ a23.c
@@ -1,12 +1,7 @@
 #include<stdio.h>
 int main(){
-    char a[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    char str;
-	int i;
-    scanf("%c",&str);
-    for(i=0;i<=26;i++){
-    	if(str==a[i])
-    	printf("%c %c %c",a[i-1],a[i],a[i+1]);
-	}
-    return 0;
+    int a,b,c;
+    scanf("%c",&b);
+    a=b-1,c=b+1;
+    printf("%c %c %c",a,b,c);
 }
